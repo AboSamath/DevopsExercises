e5f3d2164da69a90a3302c37471329b0ca65e79a
Abdou Samath YATTE <abdousamathyade@gmail.com>
2023-08-15 10:56:07 +0000

    addind the repository workflows

---
+++ MyProgram.py
@@ -0,0 +1,9 @@
+#This program will allow us to load the dataset we will use
+
+import pandas as pd
+import numpy as np
+
+def loadDataset():
+  data =  pd.read_csv("https://github.com/AboSamath/DevopsExercises/blob/main/Pokemon.csv")
+
+
